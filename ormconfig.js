5dd51d7252faaa90df6e92f7dc7248997602a9a9
P-Chan <hello@0x50.io>
2019-08-03 10:20:14 +0900

    feat: Add database settings
    charset, timezone

--- ormconfig.js
+++ ormconfig.js
@@ -9,6 +9,8 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  charset: process.env.DB_CHARSET,
+  timezone: process.env.DB_TIMEZONE,
   entities: ['./src/models/entities/*.ts'],
   migrations: ['./src/models/migrations/*.ts'],
   cli: {
